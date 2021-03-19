3073de3fe90805428f099292c5d0ac86c8a49a85
DyeslenSilva <dyeslensilva97@gmail.com>
2021-03-18 23:09:59 -0300

    Atualizando Web Flux...

--- src/main/java/es/faculdade/igts/webflux/controller/ProfessorFluxController.java
+++ src/main/java/es/faculdade/igts/webflux/controller/ProfessorFluxController.java
@@ -1,15 +1,40 @@
 package es.faculdade.igts.webflux.controller;
 
+import org.atmosphere.config.service.Get;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.faculdade.igts.model.Professor;
 import es.faculdade.igts.webflux.repo.ProfessorWebFluxRepo;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/apiFluxProfessor")
 public class ProfessorFluxController {
 
 	@Autowired 
-	private ProfessorWebFluxRepo fluxRepo;
+	private ProfessorWebFluxRepo professorWebFluxRepo;
+	
+	@PostMapping("/professor")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Mono<Professor> cadastroProfessor(@PathVariable Professor prof){
+		return professorWebFluxRepo.save(prof);
+	}
+	
+	@GetMapping("/professores")
+	public Flux<Professor> getProfessores(){
+		return professorWebFluxRepo.findAll();
+	}
+	
+	@GetMapping("/professor/{idProfessor}")
+	public Mono<Professor> getProfessor(@PathVariable Integer idProfessor){
+		return professorWebFluxRepo.findById(idProfessor);
+	}
 }
--- src/main/java/es/faculdade/igts/webflux/repo/ProfessorWebFluxRepo.java
+++ src/main/java/es/faculdade/igts/webflux/repo/ProfessorWebFluxRepo.java
@@ -1,9 +1,16 @@
 package es.faculdade.igts.webflux.repo;
 
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import es.faculdade.igts.model.Professor;
+import reactor.core.publisher.Mono;
 
 public interface ProfessorWebFluxRepo extends R2dbcRepository<Professor, Integer> {
 
+	
+	
+	public Mono<Professor> updateProfessor( Integer idProfessor);
+		
+	
 }
