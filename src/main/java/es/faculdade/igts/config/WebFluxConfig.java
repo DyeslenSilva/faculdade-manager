037102b9a60415cee2cdb4db1c3f8b6106b626b1
DyeslenSilva <dyeslensilva97@gmail.com>
2021-03-19 11:31:13 -0300

    adicionando novas configurações....

--- src/main/java/es/faculdade/igts/config/WebFluxConfig.java
+++ src/main/java/es/faculdade/igts/config/WebFluxConfig.java
@@ -1,6 +1,7 @@
 package es.faculdade.igts.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -8,4 +9,11 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer {
 	
+	   public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/api/**")
+	            .allowedOrigins("http://www.stackabuse.com")
+	            .allowedMethods("GET", "PUT", "DELETE")
+	            .allowedHeaders("testHeader")
+	            .allowCredentials(true);
+	    }
 }
---
+++ src/main/java/es/faculdade/igts/security/WebFluxSecurity.java
@@ -0,0 +1,23 @@
+package es.faculdade.igts.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class WebFluxSecurity {
+	
+	@Bean
+    public SecurityWebFilterChain springSecurityFilterChain(
+      ServerHttpSecurity http) {
+        http.csrf().disable()
+          .authorizeExchange()
+          .pathMatchers(HttpMethod.GET, "/resource/").hasRole("ADMIN")
+          .pathMatchers("/**").permitAll()
+          .and()
+          .httpBasic();
+        return http.build();
+    }
+}
